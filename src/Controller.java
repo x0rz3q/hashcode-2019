85d9f027d7cd0fa72ffb22653f16c0da11f51b5b
J.M. Thiessen <jacob@x0rz3q.com>
2019-02-28 19:19:26 +0100

    Add basic parser

--- src/Controller.java
+++ src/Controller.java
@@ -2,14 +2,14 @@ import java.io.File;
 
 public class Controller {
     public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
+        if (args.length < 1) {
             throw new Exception("No valid file given!");
         }
 
-        if (!(new File(args[1]).exists())) {
-            throw new Exception("File " + args[1] + " does not exist!");
+        if (!(new File(args[0]).exists())) {
+            throw new Exception("File " + args[0] + " does not exist!");
         }
 
-        Parser parser = new Parser(args[1]);
+        Parser parser = new Parser(args[0]);
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,5 +1,37 @@
-public class Parser {
-    public Parser(String file) throws Exception {
+import java.io.File;
+import java.util.*;
 
+public class Parser {
+    private int total;
+    private HashMap<String, Integer> tags = new HashMap<>();
+    private List<Picture> pictures = new ArrayList<>();
+
+    public Parser(String file) throws Exception {
+        Scanner scanner = new Scanner(new File(file));
+        this.total = scanner.nextInt();
+        int tagIndex = 0;
+        int id = 0;
+
+        while(scanner.hasNext()) {
+            String orientation = scanner.next();
+
+            int amount = scanner.nextInt();
+            List<Integer> tags = new ArrayList<>();
+
+            for (int i = 0; i < amount; i++) {
+                String tag = scanner.next();
+
+                if (this.tags.containsKey(tag)) {
+                    tags.add(this.tags.get(tag));
+                } else {
+                    this.tags.put(tag, tagIndex);
+                    tags.add(tagIndex);
+                    tagIndex++;
+                }
+
+                // Direction true = horizontal
+                this.pictures.add(new Picture(orientation.equals("H"), id++, tags));
+            }
+        }
     }
 }
