324e2e50b9de8e4ad9bf9a34a6335ac0568ae263
Nico <klaassen.nico@gmail.com>
2019-02-28 19:45:57 +0100

    addSlide

--- src/SlideShow.java
+++ src/SlideShow.java
@@ -14,14 +14,15 @@ public class SlideShow {
         this.slides = slides;
     }
 
+    void addSlide(Slide s) {
+        this.slides.add(s);
+    }
+
     void output(String location) throws Exception {
         FileWriter writer = new FileWriter(location);
         int n = this.slides.size();
         writer.write(n + lineSeperator);
 
-    void addSlide(Slide s) {
-        this.slides.add(s);
-    }
         // TODO: write n, write newline
         for (Slide s: this.slides) {
             writer.write(s.toString() + lineSeperator);
