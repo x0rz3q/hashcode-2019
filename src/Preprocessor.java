d42112579999a87224563b507b28deb3b2d3cd49
HiddeKoerts <hiddekoerts@live.nl>
2019-02-28 20:08:45 +0100

    Implemented preprocessor (creates all slides)

---
+++ src/Preprocessor.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+
+public class Preprocessor {
+    //
+    static ArrayList<Slide> preprocess(HashSet<Picture> verticles, HashSet<Picture> horizontals) {
+        ArrayList<Slide> result = new ArrayList<>();
+
+        ArrayList<Picture> pictures = new ArrayList<>();
+        for (Picture p : verticles) {
+            pictures.add(p);
+        }
+        class picComparator implements Comparator<Picture> {
+
+            @Override
+            public int compare(Picture picture, Picture t1) {
+                return picture.getSize() - t1.getSize();
+            }
+        }
+        Collections.sort(pictures, new picComparator());
+
+        while (pictures.size() > 1) {
+            result.add(new Slide(pictures.remove(0), pictures.remove(pictures.size() - 1)));
+        }
+        if (pictures.size() == 1) {
+            result.add(new Slide(pictures.get(0)));
+        }
+
+        for (Picture p : horizontals) {
+            result.add(new Slide(p));
+        }
+
+        return result;
+    }
+
+}
