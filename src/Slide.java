b46f22790f0345b590cc62cc8b7a9ab607f8a0d3
BenjaminRodatz <b.p.rodatz@student.tue.nl>
2019-02-28 19:05:22 +0100

    added Slide

---
+++ src/Slide.java
@@ -0,0 +1,12 @@
+public class Slide {
+    Picture[] pictures;
+
+    public Slide(Picture p1) {
+        pictures = new Picture[] {p1};
+    }
+
+
+    public Slide(Picture p1, Picture p2) {
+        pictures = new Picture[] {p1, p2};
+    }
+}
